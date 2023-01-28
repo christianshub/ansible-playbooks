561767faff913dfe319b2c072a236a69dd7d8d61
Your Name <you@example.com>
2023-01-28 19:28:02 +0100

    Disable ASLR

--- windows/apply_settings.ps1
+++ windows/apply_settings.ps1
@@ -1,5 +1,5 @@
 $settings_dir = 'settings'
 Invoke-Expression -Command ('.\{0}\vscode_extensions.ps1' -f $settings_dir)
-
-# Set registries
-Start-Process -FilePath (".\{0}\registries.bat" -f $settings_dir) -Verb runas
+Start-Process -FilePath (".\{0}\ui.bat" -f $settings_dir) -Verb runas
+Start-Process -FilePath (".\{0}\disable-ASLR.bat" -f $settings_dir) -Verb runas
+Start-Process -FilePath (".\{0}\disable-gamebar.bat" -f $settings_dir) -Verb runas
